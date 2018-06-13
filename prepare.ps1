5b644df599bdffaf3d9d581b254c56b397877d41
phamtienhung <firzen.hung@gmail.com>
2018-06-13 15:02:39 +0700

    Update prepare.ps1

--- prepare.ps1
+++ prepare.ps1
@@ -5,8 +5,5 @@ choco install maven --version 3.5.3 -y
 choco install jdk8 --version 8.0.172 -y
 choco install git --version 2.17.1.2 -y
 choco install docker-for-windows --version 18.03.1.17513 -y
-#Pull mailhog, java images 
-docker pull mailhog/mailhog
-docker pull java:7
 #Enable hyper-v
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
