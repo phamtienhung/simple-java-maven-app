8496570725f627f8e47edfc9655bc54829bf82f7
phamtienhung <firzen.hung@gmail.com>
2018-06-13 17:00:44 +0700

    Delete uninstall.ps1

--- uninstall.ps1
+++
@@ -1,7 +0,0 @@
-choco uninstall maven --version 3.5.3 -y
-choco uninstall jdk8 --version 8.0.172 -y
-choco uninstall git.install -y
-choco uninstall git --version 2.17.1.2 -y
-choco uninstall jenkins --version 2.89.2 -y
-choco uninstall docker-for-windows --version 18.03.1.17513 -y
-Disable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V-All
