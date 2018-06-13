f33606c648c801a5a8455145439d0b7e1d989e1a
phamtienhung <firzen.hung@gmail.com>
2018-06-13 15:17:05 +0700

    Update prepare.ps1

--- prepare.ps1
+++ prepare.ps1
@@ -5,5 +5,6 @@ choco install maven --version 3.5.3 -y
 choco install jdk8 --version 8.0.172 -y
 choco install git --version 2.17.1.2 -y
 choco install docker-for-windows --version 18.03.1.17513 -y
+choco install jenkins --version 2.89.2 -y
 #Enable hyper-v
 Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V -All
