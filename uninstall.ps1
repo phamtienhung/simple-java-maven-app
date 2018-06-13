0eb0fd3311dcbaddcdc21d6165caaf0b5909db65
phamtienhung <firzen.hung@gmail.com>
2018-06-13 15:05:25 +0700

    Create uninstall.ps1

---
+++ uninstall.ps1
@@ -0,0 +1,4 @@
+choco uninstall maven --version 3.5.3 -y
+choco uninstall jdk8 --version 8.0.172 -y
+choco uninstall git --version 2.17.1.2 -y
+choco uninstall docker-for-windows --version 18.03.1.17513 -y
